562d7c3d099288ac74ad684c9b8481e8a55e6919
Josue19Pillajo <84783236+Josue19Pillajo@users.noreply.github.com>
2021-06-29 11:13:56 -0500

    Update segundo.java

--- src/prueba/segundo.java
+++ src/prueba/segundo.java
@@ -1,43 +1,33 @@
-
-package test;
-
-
-public class segundo {
-    import  dominio.Punto ;
-import static  java.lang.Math.pow ;
-import static  java.lang.Math.sqrt ;
-import  java.util.Scanner ;
-
 público  de clase  TestPunto {
 
      datos del Scanner estático =  nuevo  Scanner ( System . in);
 
-    public static void main(String[]args ) {
-        System.out.println ( " Cuantos movientos desea realizar ?: " );
-        int n =0;
-        while (n <1) {
-            n = datos.nextInt ();
+    public  static  void  main ( String [] args ) {
+        Sistema . fuera . println ( " Cuantos movientos desea realizar ?: " );
+        int n =  0 ;
+        while (n <  1 ) {
+            n = datos . nextInt ();
         }
-        Punto[] punto = new Punto [n];
+        Punto [] punto =  nuevo  Punto [n];
         llenaMovimientos (punto);
 
     }
 
-    public  static  void  llenaMovimientos (Punto  pnt []) {
+    public  static  void  llenaMovimientos ( Punto  pnt []) {
         int dirX =  0 ;
         int dirY =  0 ;
-        double distancia;
+        doble distancia;
         int numMovimiento;
-        pnt[0]= nuevo Punto(dirX, dirY);
-        System.out.println(" Coordenada Inicial");
-        System.out.println(pnt [0]);
+        pnt [ 0 ] =  nuevo  Punto (dirX, dirY);
+        Sistema . fuera . println ( " Coordenada Inicial " );
+        Sistema . fuera . println (pnt [ 0 ]);
 
-        for(int i=0; i< pnt.longitud; i ++ ) {
-            datos.nextInt();
-            System.out.println( " " );
-            System.out.println( " Ingrese la distancia del movimiento No " +(i+1));
-            numMovimiento= datos.nextInt ();
-            int valorEntero= (int) Math.piso(Matemáticas.aleatorio()*(4-1+1)+1);
+        para ( int i =  0 ; i < pnt . longitud; i ++ ) {
+            datos . Proxima linea();
+            Sistema . fuera . println ( " " );
+            Sistema . fuera . println ( " Ingrese la distancia del movimiento No "  + (i +  1 ));
+            numMovimiento = datos . nextInt ();
+            int valorEntero = ( int ) Math . piso ( Matemáticas . aleatorio () * ( 4  -  1  +  1 ) +  1 );
             switch (valorEntero) {
                 caso  1 :
                     dirX = dirX - numMovimiento;
